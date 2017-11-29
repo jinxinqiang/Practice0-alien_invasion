3c91bba281aec3ba3f6ae12cda06b66583df914f
jinxinqiang <wind_dragon@163.com>
2017-11-29 21:51:09 +0800

    调整飞船的速度

--- ship.py
+++ ship.py
@@ -12,7 +12,7 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         # 在飞船的属性center中存储小数值
-        #self.center = float(self.rect.centerx)
+        self.center = float(self.rect.centerx)
         # 移动标志
         self.moving_right = False
         self.moving_left = False
@@ -21,9 +21,9 @@ class Ship():
         """根据移动标志调整飞船的位置"""
         # 更新飞船的center值，而不是rect
         if self.moving_right:
-            self.rect.centerx += 1
+            self.rect.centerx += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.rect.centerx -= self.ai_settings.ship_speed_factor
         # 根据self.center更新rect对象
         #self.rect.centerx = self.center
 
